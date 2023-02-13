a2d80956244f92eafadeeb45193845b1022bdcad
AnneDuivenvoorden <anneduivenvoorden@hotmail.com>
2023-02-13 12:36:15 +0100

    plots new

--- plottingHedge.R
+++ plottingHedge.R
@@ -7,15 +7,18 @@ library(xlsx)
 path = "/Users/anneduivenvoorden/Documents/Msc Quantitative Finance/Seminar Financial Case Studies/Seminar-QF/Data"
 desired_cf = read_excel(paste(path, "/SimulatedCFnRates.xlsx", sep = ""), sheet = 'Desired CF')
 sim_cf = read_excel(paste(path, "/SimulatedCFnRates.xlsx", sep = ""), sheet = 'Simulated CF')
-hedge = read_excel(paste(path, "/zcb elastic 0.9 hedge.xlsx", sep = ""), sheet = 1)
+hedge_margin = read_excel(paste(path, "/zcb margin hedge.xlsx", sep = ""), sheet = 1)
+hedge_value = read_excel(paste(path, "/zcb value hedge.xlsx", sep = ""), sheet = 1)
 
 desired_cf = desired_cf[,2]
 sim_cf = sim_cf[,2:101]
-hedge = hedge[,2]
+hedge_margin = hedge_margin[,2]
+hedge_value = hedge_value[,2]
 
 # PREP FOR PLOT
 desired_cf = as.numeric(unlist(desired_cf))
-hedge = as.numeric(unlist(hedge))
+hedge_margin = as.numeric(unlist(hedge_margin))
+hedge_value = as.numeric(unlist(hedge_value))
 # list with months
 m = c(1:120)
 # basic plot with only simulated and desired CF
@@ -36,9 +39,11 @@ for(i in 1:100){
 # plot desired CF
 lines(m, desired_cf, type = 'l', col = 'black', lwd = 1)
 # plot hedge
-lines(m, hedge, type = 'l', col = 'darkgreen', lwd = 2)
-legend("topright", legend = c("Cash flow w/o prepayments", "Cash flow of hedge", "Simulated cash flows"),
-       col = c("black", "darkgreen", "azure3"), lty=1:2, cex = 0.8)
+lines(m, hedge_value, type = 'l', col = 'blue3', lwd = 2)
+lines(m, hedge_margin, type = 'l', col = 'darkgreen', lwd = 2)
+legend("topright", legend = c("Cash flow w/o prepayments", "Cash flow of margin hedge", 
+                              "Cash flow of value hedge", "Simulated cash flows"),
+       col = c("black", "darkgreen", "blue3", "azure3"), lty=1:2, cex = 0.8)
 
 # zoomed in plot
 plot(NULL, xlim=c(0,120), ylim=c(-30000,42000), yaxt="n", ylab="Cash flow", 
@@ -58,7 +63,9 @@ for(i in 1:100){
 # plot desired CF
 lines(m, desired_cf, type = 'l', col = 'black', lwd = 1)
 # plot hedge
-lines(m, hedge, type = 'l', col = 'darkgreen', lwd = 2)
-legend("bottomright", legend = c("Cash flow w/o prepayments", "Cash flow of hedge", "Simulated cash flows"),
-       col = c("black", "darkgreen", "azure3"), lty=1:2, cex = 0.8)
+lines(m, hedge_value, type = 'l', col = 'blue3', lwd = 2)
+lines(m, hedge_margin, type = 'l', col = 'darkgreen', lwd = 2)
+legend("bottomright", legend = c("Cash flow w/o prepayments", "Cash flow of margin hedge", 
+                                 "Cash flow of value hedge", "Simulated cash flows"),
+       col = c("black", "darkgreen", "blue3", "azure3"), lty=1:2, cex = 0.8)
 
